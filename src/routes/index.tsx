adc901ac47d1207a1399f734b5a710a7de1ed9cf
TiagoOliverDev <paulloholiveira77@gmail.com>
2023-04-24 22:54:31 -0300

    feat: adicionando react router dom v6

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,11 @@
-export { };
+import { Routes, Route, Navigate } from "react-router-dom"
+
+export const AppRoutes = () => {
+
+    return(
+        <Routes>
+            <Route path="/pagina-inicial" element={<p>Página inicial</p>}></Route>
+            <Route path="*" element={<Navigate to={"/pagina-inicial"} ></Navigate>}></Route> //* serve para ser uma rota padrão que o react vai acessar, caso não exista outra ou não encontre... Serve para tratar erros de nomeclatura de url
+        </Routes>
+    )
+}
