b05685084e7f76ed6e7c7f3d224944f6f8e60fdd
TiagoOliverDev <paulloholiveira77@gmail.com>
2023-04-25 23:39:00 -0300

    feat: implementando tema light

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,8 @@ export const AppRoutes = () => {
 
     return(
         <Routes>
-            <Route path="/pagina-inicial" element={<Button>Teste</Button>}></Route>
+            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" >Teste</Button>}></Route>
+            
             <Route path="*" element={<Navigate to={"/pagina-inicial"} ></Navigate>}></Route> //* serve para ser uma rota padrão que o react vai acessar, caso não exista outra ou não encontre... Serve para tratar erros de nomeclatura de url
         </Routes>
     )
