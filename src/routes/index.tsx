9c156147e5192cbbf13a293a12693ca50e6708b7
TiagoOliverDev <paulloholiveira77@gmail.com>
2023-05-16 23:05:23 -0300

    Feat: usando o context criado

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@mui/material"
 import { Routes, Route, Navigate } from "react-router-dom"
+import { useAppThemeContext } from "../shared/contexts"
 
 export const AppRoutes = () => {
 
+    const { toggleTheme } = useAppThemeContext();
+
     return(
         <Routes>
-            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" >Teste</Button>}></Route>
+            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" onClick={toggleTheme}>Toggle theme</Button>}></Route>
                        
             <Route path="*" element={<Navigate to={"/pagina-inicial"} ></Navigate>}></Route> 
             {/* o path="*" serve para ser uma rota padrão que o react vai acessar, caso não exista outra ou não encontre... Serve para tratar erros de nomeclatura de url */}
