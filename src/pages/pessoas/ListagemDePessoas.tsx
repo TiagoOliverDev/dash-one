8261d75a8fd1b06b2a89fbea02e0cdf52a715edb
TiagoOliverDev <paulloholiveira77@gmail.com>
2023-07-03 16:40:55 -0300

    feat: criando tabela  de listagem

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,14 +1,20 @@
 import { useSearchParams } from "react-router-dom"
 import { FerramentasDaListagem } from "../../shared/components"
 import { LayoutBaseDePagina } from "../../shared/layouts"
-import { useEffect, useMemo } from "react";
-import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
+
+import { useEffect, useMemo, useState } from "react";
+import { IListagemPessoa, PessoasService } from "../../shared/services/api/pessoas/PessoasService";
 import { useDebounce } from "../../shared/hooks";
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 
 
 export const ListagemDePessoas: React.FC = () => {
     const [searchParams, setSearchParams] = useSearchParams();
-    const { debounce } = useDebounce(3000);
+    const { debounce } = useDebounce();
+
+    const [rows, setRows] = useState<IListagemPessoa[]>([]);
+    const [totalCount, setTotalCount] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
 
     const busca = useMemo(() => {
         return searchParams.get('busca') || '';
@@ -16,16 +22,21 @@ export const ListagemDePessoas: React.FC = () => {
 
     
     useEffect(() => {
+        setIsLoading(true) //garantindo que irá consultar o backend
         // Toda consulta no backend deve ser realizada dentro de um useEffect pq qualquer caso fique fora do useEffect qualquer mudança de estado ele irá consultar o backend novamente e isso causa problemas de performance
         // Já no useEffecct conseguimos fazer essa consulta uma única vez em momentos específicos.
         debounce(() => {
             PessoasService.getAll(1, busca)
                 .then((result) => {
+                    setIsLoading(false) //assim que realizou a consulta já seta false aqui para parar de mostrar o feedback para o user
+
                     if(result instanceof Error){
                         //caso der erro na consulta     
                         alert(result.message);
                     }else{              
                         console.log(result)
+                        setRows(result.data);
+                        setTotalCount(result.totalCount)
                     }
                 })          
         })
@@ -43,7 +54,28 @@ export const ListagemDePessoas: React.FC = () => {
                 />
             }
         >
-        
+            <TableContainer component={Paper} variant="outlined" sx={{ m: 1, width: 'auto' }}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Ações</TableCell>
+                            <TableCell>Nome completo</TableCell>
+                            <TableCell>Email</TableCell>
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        {rows.map(row => (
+                            <TableRow key={row.id}>
+                                <TableCell>Ações</TableCell>
+                                <TableCell>{row.nomeCompleto}</TableCell>
+                                <TableCell>{row.email}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            
         </LayoutBaseDePagina>
     )
 }
--- src/shared/hooks/UseDebounce.ts
+++ src/shared/hooks/UseDebounce.ts
@@ -1,8 +1,7 @@
 import { useCallback, useRef } from "react"
 
 
-
-export const useDebounce = (delay = 300, notDelayInFirstTime = true) => {
+export const useDebounce = (delay = 1000, notDelayInFirstTime = true) => {
     // Problema: temos um campo de busca na rotaa /pessoas e a cada letra digitada a aplicação consulta o backend
     // o useDebounce é criado para resolver esse problema
     // o isFirstTime serve para os casos em que o usuário entrou a primeira vez na página, nesse caso é importante consultar o backend ao mesmo tempo que entra na página
